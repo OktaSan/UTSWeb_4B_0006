1bd2174eff7d32c40baa3b45b5845f26d2892693
Okta Putra Ramadhan <oktaxd88@gmail.com>
2026-05-29 06:21:12 +0700

    Memperbaiki style yang overide dan juga perbaiki trehadap scripts dalam body

--- js/main.js
+++ js/main.js
@@ -4,10 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     filterButtons.forEach(btn => {
         btn.addEventListener("click", () => {
+            filterButtons.forEach(b => b.classList.remove('active'))
+
+            btn.classList.add("active")
+
             const filterValue = btn.getAttribute("data-filter")
 
             galeryItems.forEach(item => {
-                if(filterValue === "semua" || item.classList.contains(filterValue)) {
+                if (filterValue === "semua" || item.classList.contains(filterValue)) {
                     item.style.display = "block";
                 } else {
                     item.style.display = "none";
